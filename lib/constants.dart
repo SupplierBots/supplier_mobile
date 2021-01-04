bc9568c0b977313140d5248d1fbf6fe6f8fdcdb8
Jurek Sek <sekjurek@gmail.com>
2021-01-04 18:23:06 +0100

    screens

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 
-const kColor = Color(0xFFC95BA3);
+const kPinkColor = Color(0xFFC95BA3);
+const kVioletColor = Color(0xFF9253C1);
+const kLightPurple = Color(0xFFF1E3FF);
+const kBackgroundColor = Color(0xFF1B191C);
+const kSecondaryBackground = Color(0xFF242225);
+const kTetiaryBackground = Color(0xFF282629);
+const kDarkGrey = Color(0xFF747273);
+const kLighGrey = Color(0xFFB4B1B2);
---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: FlatButton(
+          child: Text('View Details'),
+          onPressed: () {
+            Navigator.pushNamed(
+              context,
+              '/details/1',
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
