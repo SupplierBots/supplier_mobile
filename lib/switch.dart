aed1d03de3d6f2ecdb6b42b60e8ccdddd4452644
Jurek Sek <sekjurek@gmail.com>
2021-01-05 03:11:09 +0100

    checkbox

--- lib/switch.dart
+++ lib/switch.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:supplier_mobile/constants.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 class Switcher extends StatefulWidget {
   @override
@@ -13,15 +14,34 @@ class _SwitcherState extends State<Switcher> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: CupertinoSwitch(
-        value: isSwitched,
-        onChanged: (value) {
-          setState(() {
-            isSwitched = value;
-            print(isSwitched);
-          });
-        },
-        activeColor: kPinkColor,
+      child: Row(
+        children: [
+          FlutterSwitch(
+            height: 20.0,
+            width: 40.0,
+            padding: 1.0,
+            toggleSize: 20.0,
+            borderRadius: 10.0,
+            value: isSwitched,
+            onToggle: (value) {
+              setState(() {
+                isSwitched = value;
+                print(isSwitched);
+              });
+            },
+            activeColor: kPinkColor,
+          ),
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              'Giovana',
+              style: TextStyle(
+                color: kLighGrey,
+                fontSize: 23,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
